f619861585758485cf34f0142b12ae60cdcbdcc5
lukaJashi <110906385+lukaJashi@users.noreply.github.com>
2022-10-15 14:19:57 +0400

    Add files via upload

---
+++ printface/ConsoleApp2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+#pragma warning disable
+
+
+namespace PrintFace
+{
+    public static class Program
+    {
+
+        public static void Main()
+        {
+            Console.WriteLine("Hello, world! ");
+            SayHelloUser("User");
+            PrintFace();
+            Console.ReadLine();
+        }
+
+
+        public static void SayHelloUser(string userName)
+        {
+            Console.WriteLine($"Hello, {userName}!");
+
+            //throw new NotImplementedException();
+        }
+
+
+        public static void PrintFace()
+        {
+            string a = " +\"\"\"\"\"+";
+            string b = "(| o o |)";
+            string c = " |  ^  |";
+            string d = " | '-' |";
+            string e = " +-----+";
+
+
+
+
+            Console.WriteLine(a + "\n" + b + "\n" + c + "\n" + d + "\n" + e);
+
+        }
+    }
+}
